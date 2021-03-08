ef4b13502d46f96e5c5635e4ac3e4e3777c99211
Marcis Zoldners <marcis.zoldners@gmail.com>
2021-03-08 22:47:13 +0200

    Liking tweets

--- routes/api.php
+++ routes/api.php
@@ -3,3 +3,6 @@
 Route::get('/timeline', 'Api\Timeline\TimelineController@index');
 
 Route::post('/tweets', 'Api\Tweets\TweetController@store');
+
+Route::post('/tweets/{tweet}/likes', 'Api\Tweets\TweetLikeController@store');
+Route::delete('/tweets/{tweet}/likes', 'Api\Tweets\TweetLikeController@destroy');
